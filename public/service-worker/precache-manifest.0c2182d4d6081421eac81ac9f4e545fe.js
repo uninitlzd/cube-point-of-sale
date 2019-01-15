5ecfba7ce9fd0c4d733043530e10e60a80bedaa3
Alfredo Eka <alfredoeka@outlook.com>
2019-01-15 14:00:53 +0700

    Add asset file

---
+++ public/service-worker/precache-manifest.0c2182d4d6081421eac81ac9f4e545fe.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = [
+  {
+    "url": "/fonts/vendor/element-ui/lib/theme-chalk/element-icons.woff?2fad952a20fbbcfd1bf2ebb210dccf7a"
+  },
+  {
+    "url": "/fonts/vendor/element-ui/lib/theme-chalk/element-icons.ttf?6f0a76321d30f3c8120915e57f7bd77e"
+  },
+  {
+    "revision": "a2ace524d84413c24180",
+    "url": "//js/vendor.js"
+  },
+  {
+    "revision": "64ae476d7193e3458570",
+    "url": "//js/manifest.js"
+  },
+  {
+    "revision": "6b302d0b2a8e771722d5",
+    "url": "//js/app.js"
+  },
+  {
+    "revision": "6b302d0b2a8e771722d5",
+    "url": "//css/app.css"
+  },
+  {
+    "revision": "76a78d6db9ef5c49377e7e01a07975e6",
+    "url": "/"
+  }
+];
